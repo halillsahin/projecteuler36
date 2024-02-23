5c4f14682a031fc5b3b86f51b3af35089d3b4a71
halil <156532556+halillsahin@users.noreply.github.com>
2024-02-23 18:14:21 +0300

    Update projecteuler36.py

--- projecteuler36.py
+++ projecteuler36.py
@@ -1,5 +1,5 @@
 a=[]
 for i in range(1,1000000):
-    if str(i)[::-1]==str(i)and str(bin(i))[2::]==str(bin(i))[:1:-1]:
+    if str(i)[::-1]==str(i) and str(bin(i))[2::]==str(bin(i))[:1:-1]:
         a.append(i)
 print(sum(a))
